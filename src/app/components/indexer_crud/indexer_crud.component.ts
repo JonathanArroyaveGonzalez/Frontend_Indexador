c9e51942b11f65eeff96c1fc11980e457406d880
JonathanArroyaveGonzalez <arroyave3000g@gmail.com.com>
2024-11-25 00:55:43 -0500

    feat: initial single page aplication

---
+++ src/app/components/indexer_crud/indexer_crud.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-indexer_crud',
+  templateUrl: './indexer_crud.component.html',
+  styleUrls: ['./indexer_crud.component.css']
+})
+export class Indexer_crudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
