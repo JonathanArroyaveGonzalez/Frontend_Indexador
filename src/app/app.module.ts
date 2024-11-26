09d49e6dc7e74c84741765e59575fb7648d5022e
BGHcar <bryan.1701720431@ucaldas.edu.co>
2024-11-25 20:03:23 -0500

    prueba

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([]) // Agrega rutas aquí si es necesario
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
