e9b060cb3bf6c448dac9ed78d03ebcd1962e92cc
JonathanArroyaveGonzalez <arroyave3000g@gmail.com.com>
2024-12-09 02:09:38 -0500

    chore: update route login

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8000/genome/',
-  apiRegister: 'http://127.0.0.1:5000/'
+  apiRegister: 'http://localhost:8000/login/'
 };
